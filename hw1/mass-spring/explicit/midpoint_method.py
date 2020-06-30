2351a2b409fe4868addb648a4b0834e89b301574
g1n0st <g1n0st@live.com>
2020-06-30 11:37:57 +0800

    Support explicit midpoint method (passed visual test)

---
+++ hw1/mass-spring/explicit/midpoint_method.py
@@ -0,0 +1,53 @@
+# author: g1n0st
+# Mid-point Method for Mass-Spring System
+import sys
+sys.path.append('../')
+from mass_spring_framework import *
+
+v2 = ti.Vector(2, dt = ti.f32, shape = max_particles)
+a1 = ti.Vector(2, dt = ti.f32, shape = max_particles)
+a2 = ti.Vector(2, dt = ti.f32, shape = max_particles)
+    
+@ti.kernel
+def substep():
+    # compute force and new velocity
+    n = num_particles[None]
+
+    for i in range(n):
+        v[i] *= ti.exp(-dt * damping[None]) # damping
+
+    #1
+    for i in range(n):
+        a1[i] = gravity * particle_mass
+        for j in range(n):
+            if rest_length[i, j] != 0:
+                x_ij = x[i] - x[j]
+                a1[i] += -stiffness[None] * (x_ij.norm() - rest_length[i, j]) * x_ij.normalized()
+
+    #2
+    for i in range(n):
+        v2[i] = v[i] + (dt / 2) * a1[i]
+        
+    for i in range(n):
+        a2[i] = gravity * particle_mass
+        for j in range(n):
+            if rest_length[i, j] != 0:
+                x_ij = x[i] - x[j] + (dt / 2) * (v[i] - v[j])
+                a2[i] += -stiffness[None] * (x_ij.norm() - rest_length[i, j]) * x_ij.normalized()
+
+    collide_with_ground()
+
+    for i in range(n):
+        x[i] += dt * v2[i]
+        v[i] += dt * a2[i] / particle_mass
+
+init_mass_spring_system()
+
+while True:
+    process_input()
+
+    if not paused[None]:
+        for step in range(10):
+            substep()
+
+    process_output()
